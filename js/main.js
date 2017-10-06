d598b995ffbc3ce29f5604f627849a5d86277c78
paulmatafox <freelancermt.fox@gmail.com>
2017-10-06 17:50:59 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function(){
+ 
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+ 
+        $('ul.tabs li').removeClass('active');
+        $('.tab-content').removeClass('active');
+ 
+        $(this).addClass('active');
+        $("#"+tab_id).addClass('active');
+    })
+ 
+})
